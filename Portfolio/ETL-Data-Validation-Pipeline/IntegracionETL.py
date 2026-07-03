2356d6bf488dd77625b6c9c127ae6e99babd8e7c
Vane <vaneclaudg@gmail.com>
2026-07-03 17:49:23 -0600

    featduplicated functon added docs updated with new TC

--- Portfolio/ETL-Data-Validation-Pipeline/IntegracionETL.py
+++ Portfolio/ETL-Data-Validation-Pipeline/IntegracionETL.py
@@ -2,16 +2,18 @@
 import pandas as pd
 import os
 import sys
+from collections import Counter
 file_name="hojatest.xlsx"
-sheet_name="TC06"
+sheet_name="TC09"
 rows=50
-data_list: list[str]=['date','campaign','channel','impressions','total_click','spend','video_views','conversion']
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 file_path = os.path.join(current_dir,"assets",file_name)
-data=pd.read_excel(file_path, sheet_name=sheet_name, nrows=rows)
+data=pd.read_excel(file_path, sheet_name=sheet_name,header=None, nrows=rows)
 
 def read_header(data):
+    data_list: list[str]=['date','campaign','channel','impressions','total_click','spend','video_views','conversion']
+
     header_found=False
     coincidencia=0
     max_coincidencia=0
@@ -29,20 +31,63 @@ def read_header(data):
                 if coincidencia > max_coincidencia:
                  max_coincidencia=coincidencia
                  header_found=True
-                 header=index    
+                 header=index   
+        else:
+            print("No se encontro ninguna coincidencia de header") 
+            sys.exit(1)
         
-    return header, header_found
-        
-header,_=read_header(data)
-data=pd.read_excel(file_path, sheet_name=sheet_name,header=header)
-print(header)
-data.info()
+    return header, header_found,max_coincidencia
+header,header_found,max_coincidencia=read_header(data)
+print(f" valor de Header found {header_found}")
+print(f"posicion de header {header}")
+print(f"Valor de max coincidencia {max_coincidencia}")
 
+data= pd.read_excel(file_path, header=None, skiprows=header, nrows=1).iloc[0].tolist()
+#data=pd.read_excel(file_path, sheet_name=sheet_name,header=header)
+#print(header)
+#data.info()
+print (f"El valor de data transformado a lista es {data}")
+def header_filter(data):
+    is_clean=True
+    trash_data=[]
+    clean_data=[]
+    for value in data:
+        if  value in data_list:
+            clean_data.append(value)
+        else:
+            trash_data.append(value)
+    
+    if trash_data:
+        is_clean=False
+
+    return is_clean,trash_data,clean_data
+is_clean,trash_data,clean_data=header_filter(data)
+print(f"Valor de is clean {is_clean}")
+print(f"Valor de trash data {trash_data}")
+print(f"Valor de clean data lista {clean_data}")
+
+print(f"Valor de clean data lista entrando a validacion de duplicados{clean_data}")
+def duplicated(clean_data):
+    is_duplicated=False
+    conteo=Counter(clean_data)
+    dup_list=[]
+    
+    for elemento,cantidad in conteo.items():
+        if cantidad >1:
+            dup_list.append(elemento)
+            is_duplicated=True
+    return is_duplicated, dup_list
+
+is_duplicated,dup_list=duplicated(clean_data)
+print(f"Valor de si es duplicados boleano es {is_duplicated}")
+print(f"Valor de cuantos hay duplicados en la lista es {dup_list}")
+
+""""
 try:
     
     
     if set(data_list).issubset(data.columns):
-        if data.columns.str.contains(r'\.\d+$').any():
+        #if data.columns.str.contains(r'\.\d+$').any():
             duplicated= set(data.columns)-set(data_list)
             dup_list=[]
             for item in duplicated:
@@ -59,13 +104,13 @@ try:
             print("Tus datos estan compeltos")
             data.info()
 
-    elif len(set(data_list).intersection(data.columns)) / len(data_list) > 0:#added
+    elif len(set(data_list).intersection(data.columns)) / len(data_list) > 0:added
        missing_data= set(data_list)-set(data.columns)
        print(f"Error critico: faltan los siguientes datos {missing_data}")
        data.info()
        sys.exit(0)
 
-    elif data.columns.str.startswith('Unnamed').all():
+   # elif data.columns.str.startswith('Unnamed').all():
      
         found=False
         header=0 
@@ -80,7 +125,7 @@ try:
             data=pd.read_excel(file_path, sheet_name=sheet_name,header=header)
 
             if set(data_list).issubset(data.columns):
-                if data.columns.str.contains(r'\.\d+$').any():
+             #   if data.columns.str.contains(r'\.\d+$').any():
                     duplicated= set(data.columns)-set(data_list)
                     dup_list=[]
                     for item in duplicated:
@@ -117,11 +162,11 @@ except FileNotFoundError:
 
 #data=data.loc[:,~data.columns.str.startswith('Unnamed')]
 #data.drop(columns=data.columns[data.columns.str.startswith('Unnamed')])
-#print(data) imprime los datos de la hoja de la hoja que se subio
-#data.info() imprime que tipo de valores subi por cada columna es decir int64, channel str,date tipo datetime ect
+print(data) imprime los datos de la hoja de la hoja que se subio
+data.info() imprime que tipo de valores subi por cada columna es decir int64, channel str,date tipo datetime ect
 
 
-"""
+
  transformando los datos de lectura al valor que debe corresponder de cada uno
 data['date'] = pd.to_datetime(file_path['date'])
 data['campaign'] = file_path['campaign'].astype('string')
